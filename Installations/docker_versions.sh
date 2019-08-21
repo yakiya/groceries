fa5decc3e7b33ffc391d1cb5d9e30351791cd342
yakiya <jayv8088@gmail.com>
2019-08-21 21:20:57 +0800

    change on 三  8月 21 21:20:57 CST 2019

--- Installations/docker_versions.sh
+++ Installations/docker_versions.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
+#docker version check
+#auth: neo_hsu
+#version: 2.0
 
-
-read -p "select version you wanna install for example, docker-ce-18.03.0.ce: " version
-if [[ ${version} =~ "el7" ]] ; then
-    sudo yum install -y docker-ce-${version} 
-elif [ -z "${version}" ] ; then
-    sudo yum install -y docker-ce
+read -p "select version you wanna install for example, 18.03.0.ce: " version
+if [[ ${version:1:1} =~ ":" ]] ; then
+    version=${version:1}
+    sudo yum install -y docker-ce-${version:1}
+elif [[ ${version} =~ "el7" ]] ; then
+    sudo yum install -y docker-ce-${version}
 else
-    echo "[ERROR]: You MUST have to choose one VERSION!!"
+    echo "[ERROR]: Please choose one version!!"
 fi
-
- 
