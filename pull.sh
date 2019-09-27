ba1dd1c1a62a76544ce228072bd533fa2d77d74f
root <root@xavi-test-centos7-1.novalocal>
2019-09-27 05:03:03 +0000

    Fri Sep 27 05:03:03 UTC 2019

---
+++ pull.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Describe: git pull
+
+if [ $1'x' != 'x' ]; then
+  tag=$1
+else
+  tag=origin
+fi
+
+echo -e "\033[1;31m-----------------------$n----------------------------\033[0m"
+git pull $tag master
+git log -n 3
+echo
+
+echo -e "\033[1;31m-----------------------$n----------------------------\033[0m"
+git log -n 3 --oneline
+echo
+
+
+if [ $1'x' != 'x' ]; then
+  tag=$1
+else
+  tag=origin
+fi
