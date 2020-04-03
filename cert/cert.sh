ab5b094735b28849a921cb593a0e6c27f5f02a3d
root <root@hk170svr003031.phnamedns.com>
2020-04-03 11:00:18 +0800

    Fri Apr  3 11:00:18 HKT 2020

---
+++ cert/cert.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+> ./cert_warn.txt
+> ./fkd_warn.txt
+
+domains="cat /etc/xavi/A03/cert/spcl_domains.txt"
+fkd_domains="cat /etc/xavi/A03/cert/fkd_domains.txt"
+
+
+for domain in `$domains`
+    do
+      end_time=$(echo | timeout 2 openssl s_client -servername $domain -connect $domain:443 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null | awk -F '=' '{print $2}' )
+      ([ $? -ne 0 ] || [[ $end_time == '' ]]) &&  exit 10
+               
+      end_times=`date -d "$end_time" +%s `
+      current_times=`date -d "$(date -u '+%b %d %T %Y GMT') " +%s `
+                            
+      let left_time=$end_times-$current_times
+      days=`expr $left_time / 86400`
+
+      [ $days -lt 30 ] && echo -e "===== $domain =====\n\n 剩余天数: $days 天; [WARN]: 证书有效期少于30天，存在风险！\n" >> ./cert_warn.txt || echo -e "\n==== $domain ====\n\n 剩余天数: $days 天" 
+      echo ""
+done
+
+
+for fkd in `$fkd_domains`
+    do
+      end_time=$(echo | timeout 2 openssl s_client -servername $fkd -connect $fkd:443 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null | awk -F '=' '{print $2}' )
+      ([ $? -ne 0 ] || [[ $end_time == '' ]]) &&  exit 10
+               
+      end_times=`date -d "$end_time" +%s `
+      current_times=`date -d "$(date -u '+%b %d %T %Y GMT') " +%s `
+                            
+      let left_time=$end_times-$current_times
+      days=`expr $left_time / 86400`
+
+      [ $days -lt 30 ] && echo -e "===== $fkd =====\n\n 剩余天数: $days 天; [WARN]: 证书有效期少于30天，存在风险！\n" >> ./fkd_warn.txt || echo -e "\n==== $fkd ====\n\n 剩余天数: $days 天" 
+      echo ""
+done
---
+++ cert/mail.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# coding: UTF-8
+import zipfile
+import smtplib
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import datetime, timedelta, date
+today = date.today()
+receive = "xavi.m@iv66.net"
+class Mail():
+    def Send(self, _to, _cc,_bcc, _from, _subject, content,_file):
+        msg = MIMEMultipart()
+        msg['to'] = _to
+        msg['cc'] = _cc
+        msg['bcc'] = _bcc
+        msg['from'] = _from
+        msg['subject'] = _subject
+
+        msgcontent = MIMEText(content, 'html', 'utf-8')
+
+        msg.attach(msgcontent)
+
+        _From = _from
+        _To = []
+        for i in _to.split(','):
+            _To.append(i)
+        if _cc != '':
+            for i in _cc.split(','):
+                _To.append(i)
+        _To.append(i)
+        if _bcc != '':
+            for i in _bcc.split(','):
+                _To.append(i)
+        _To.append(i)
+
+        if _file:
+            fileList = _file.split(',')
+            for n in fileList:
+                attach = MIMEText(open(n, 'rb').read(), 'base64','utf-8')
+                attach['Content-Type'] = 'application/octet-stream'
+                attach['Content-Disposition'] = 'attachment; filename="%s"' % (n)
+                msg.attach(attach)
+
+        try:
+            server = smtplib.SMTP('127.0.0.1', 25)
+            server.sendmail(_From, _To, msg.as_string())
+            server.quit()
+            print("Send Mail success!!")
+        except Exception as e:
+            print(str(e))
+
+certwarn = open('/etc/xavi/A03/cert/cert_warn.txt')
+certwarn = certwarn.read()
+if certwarn == '':
+    print("no overdate domains.")
+else:
+    certwarn= "<br />".join(certwarn.split("\n"))
+    mail = Mail()
+    file = ''
+    subject = 'A03_特殊域名证书到期通知'
+    content = '''
+        <font face="Arial" size="2" color="black">
+        
+            <br>
+            {}
+            <br>
+            Best Regards,<br>
+            <br>
+            B2  <br>
+            SA/运维部/<br>
+        </font>
+        <font face="微软雅黑" size="2" color="gray">
+        *******************************************************************************************************************************************<br>
+        该邮件为机密文件，如您不是指定收件人，请将邮件删除。谢谢！<br>
+        Notice: This transmittal or attachments may be confidential. If you are not the intended receipt, you are hereby notified that you have received this transmittal  in error; any review, dissemination or copy is strictly prohibited. If you have received this transmittal in error, please notify us immediately by reply and immediately delete this message and all its attachments. Thank you.<br>
+        *******************************************************************************************************************************************<br>
+        </front>'''.format(certwarn)
+    mail.Send('{0}'.format(receive), 'better@iv66.net', '','b4@iv66.net',subject ,content ,file)
+
