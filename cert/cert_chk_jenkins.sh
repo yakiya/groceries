a858d2ef455a5d0c0ed196a11b426a06660ab196
yakiya <jayv8088@gmail.com>
2020-04-03 15:25:53 +0800

    Fri Apr  3 15:25:53 HKT 2020

---
+++ cert/cert_chk_jenkins.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Title: Special domain SSL check for Jenkins v2.0
+#Author: Xavi
+
+#===Initial====
+> /root/jenkins/walker/shell/cert_check/cert_warn.txt
+
+sed -i "s/ /\n/g" /root/jenkins/walker/shell/cert_check/spcl_domains.txt
+domains="cat /root/jenkins/walker/shell/cert_check/spcl_domains.txt"
+
+#====SSL Check====
+
+for domain in `$domains`
+    do
+      end_time=$(echo | timeout 3 openssl s_client -servername $domain -connect $domain:443 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null | awk -F '=' '{print $2}' )
+      ([ $? -ne 0 ] || [[ $end_time == '' ]]) &&  exit 10
+               
+      end_times=`date -d "$end_time" +%s `
+      current_times=`date -d "$(date -u '+%b %d %T %Y GMT') " +%s `
+                            
+      let left_time=$end_times-$current_times
+      days=`expr $left_time / 86400`
+
+      [ $days -lt 30 ] && echo -e "===== $domain =====\n\n 剩余天数: $days 天; [WARN]: 证书有效期少于30天，存在风险！\n" >> /root/jenkins/walker/shell/cert_check/cert_warn.txt || echo -e "\n==== $domain ====\n\n 剩余天数: $days 天" 
+      echo ""
+done
+
+#====send mail====
+/root/jenkins/walker/shell/cert_check/mail.py
