6dcb62cc4687b51cf7dfdf8df164a0d0f93b789d
yakiya <38606888+yakiya@users.noreply.github.com>
2021-02-04 17:44:54 +0800

    Create lotto_exe.sh
    lotto execute

---
+++ lotto_exe.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# get lotto
+
+while true
+do
+    go run lottery.go > num.list
+    grep -E "8|18" num.list > list.txt
+    grep -E "10|1" list.txt > katsu.txt
+    
+    TTL=`wc -l katsu.txt | awk '{print $1}'`
+    
+    if [ $TTL == '20' ]; then
+        break
+    else
+        continue
+     fi
+done
